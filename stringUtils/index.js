b489d2c3abaca27695d3d0ad5a1d3e6f95a73c22
Asma Rasheed <Asmarasheed4599@gmail.com>
2024-04-24 11:32:38 +0300

    added author , edited line docs

--- stringUtils/index.js
+++ stringUtils/index.js
@@ -1,22 +1,29 @@
-/* The StringUtils class provides a method to remove non-word characters from a string. */
 /**
- * A utility class for working with strings.
  * @class StringsUtils
  */
+/* The StringUtils class provides a static method to remove non-word characters from a given string. */
 class StringUtils {
-  /**
-   * @param {string} string
-   * @returns {string}
-   */
-
-  //Remove non-word chars.
+/**
+ * @author Asma Rasheed
+ * The function `removeNonWord` removes all non-alphanumeric characters from a given string.
+ * @param string - The `removeNonWord` function takes a string as input and removes any characters that
+ * are not alphanumeric, spaces, hyphens, or accented characters. The regular expression
+ * `/[^0-9a-zA-Z\xC0-\xFF \-]/g` is used to match any character that
+ * @returns The `removeNonWord` function is returning a new string where all characters that are not
+ * alphanumeric, spaces, hyphens, or accented characters are removed from the input `string`.
+ */
   static removeNonWord(string) {
     return string.replace(/[^0-9a-zA-Z\xC0-\xFF \-]/g, "");
   }
 }
 
-/* This code block is a common pattern used in Node.js environments to export modules for use in other
-files. */
+
+/* The code block `if (typeof module !== "undefined" && typeof module.exports !== "undefined") {
+  module.exports = {
+    StringUtils,
+  };
+}` is a common pattern used in Node.js environments to export modules for use in other files. */
+
 if (typeof module !== "undefined" && typeof module.exports !== "undefined") {
   module.exports = {
     StringUtils,
